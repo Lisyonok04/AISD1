a8031e965cd5ecb3ae09e8bf36f33510c4421b1d
Mary <marybelka04@gmail.com>
2024-04-03 22:20:16 +0400

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -14,3 +14,37 @@ struct Node {
 	Node(int val, Node<T>* left, Node<T>* right) :_val(val), _left(left), _right(right) {};
 };
 
+template<typename T>
+class Tree {
+private:
+    Node<T>* root;
+    
+    void clear(Node<T>* node) {
+        if (node) {
+            clear(node->_left);
+            clear(node->_right);
+            delete node;
+        }
+    }
+
+    void print(Node<T>* node) {
+        if (node) {
+            print(node->_left);
+            std::cout << node->_val << " ";
+            print(node->_right);
+        }
+    }
+
+	bool insert(Node<T>*& node, T key) {
+		if (!node) {
+			node = new Node<T>(key);
+			return true;
+		}
+		if (node->_val > key) {
+			return insert(node->_left, key);
+		}
+		else if (node->_val < key) {
+			return insert(node->_right, key);
+		}
+		return false;
+	}
