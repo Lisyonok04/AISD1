64414f9b572ae6f5c1e7aececd6a7b68f82988a3
Mary <marybelka04@gmail.com>
2024-04-03 22:57:22 +0400

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -17,23 +17,23 @@ struct Node {
 template<typename T>
 class Tree {
 private:
-    Node<T>* root;
-    
-    void clear(Node<T>* node) {
-        if (node) {
-            clear(node->_left);
-            clear(node->_right);
-            delete node;
-        }
-    }
+	Node<T>* root;
 
-    void print(Node<T>* node) {
-        if (node) {
-            print(node->_left);
-            std::cout << node->_val << " ";
-            print(node->_right);
-        }
-    }
+	void clear(Node<T>* node) {
+		if (node) {
+			clear(node->_left);
+			clear(node->_right);
+			delete node;
+		}
+	}
+
+	void print(Node<T>* node) {
+		if (node) {
+			print(node->_left);
+			std::cout << node->_val << " ";
+			print(node->_right);
+		}
+	}
 
 	bool insert(Node<T>*& node, T key) {
 		if (!node) {
@@ -48,3 +48,19 @@ private:
 		}
 		return false;
 	}
+public:
+	Tree() : root(nullptr) {}
+	Tree(const Tree<T>& other) {
+		root = copy(other.root);
+	}
+	~Tree() {
+		clear(root);
+	}
+	bool insert(T value) {
+		return insert(root, value);
+	}
+	void print() {
+		print(root);
+		std::cout << std::endl;
+	}
+};
