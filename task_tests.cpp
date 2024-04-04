556768a03bd708c4ef8ca7dfcbdd01819df089d4
Mary <marybelka04@gmail.com>
2024-04-04 22:56:38 +0400

    Update task_tests.cpp

--- task_tests.cpp
+++ task_tests.cpp
@@ -7,18 +7,26 @@ using namespace std;
 
 TEST(insert, test) {
     Tree<int> set;
-    set.insert(41);
-    set.insert(90);
-    set.insert(20);
+    set.inserter(41);
+    set.inserter(90);
+    set.inserter(20);
     set.print();
 }
 
 TEST(clear, test) {
     Tree<int> set;
-    set.insert(41);
-    set.insert(90);
-    set.insert(20);
+    set.inserter(41);
+    set.inserter(90);
+    set.inserter(20);
     set.print();
     set.eraser(41);
     set.print();
+}
+
+TEST(get_elements, test) {
+    Tree<int> set;
+    Node<int>* node1 = new Node<int>(1);
+    Node<int>* node2 = new Node<int>(4);
+    Node<int>* res = new Node<int>(2, node1, node2);
+    cout<<get_elements(res);
 }
