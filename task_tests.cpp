f256f9f688e782ae36b099d0606e931b635bffd9
Mary <marybelka04@gmail.com>
2024-04-04 23:33:39 +0400

    Update task_tests.cpp

--- task_tests.cpp
+++ task_tests.cpp
@@ -23,10 +23,20 @@ TEST(clear, test) {
     set.print();
 }
 
-TEST(get_elements, test) {
+TEST(get_element, test) {
     Tree<int> set;
     Node<int>* node1 = new Node<int>(1);
     Node<int>* node2 = new Node<int>(4);
     Node<int>* res = new Node<int>(2, node1, node2);
     cout<<get_elements(res);
+}
+
+TEST(get_element2, test) {
+    Tree<int> set;
+    Node<int>* node1 = new Node<int>(1);
+    Node<int>* node3 = new Node<int>(5);
+    Node<int>* node4 = new Node<int>(10);
+    Node<int>* node2 = new Node<int>(4, node3, node4);
+    Node<int>* res = new Node<int>(2, node1, node2);
+    cout << get_elements(res);
 }
