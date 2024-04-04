dd5b96ce10fe2e95868196af3ca882846f61293e
Mary <marybelka04@gmail.com>
2024-04-05 01:08:39 +0400

    Update task_tests.cpp

--- task_tests.cpp
+++ task_tests.cpp
@@ -40,3 +40,31 @@ TEST(get_element2, test) {
     Node<int>* res = new Node<int>(2, node1, node2);
     cout << get_elements(res);
 }
+
+TEST(contain, test) {
+    Tree<int> set;
+    Node<int>* node1 = new Node<int>(1);
+    Node<int>* node3 = new Node<int>(5);
+    Node<int>* node4 = new Node<int>(10);
+    Node<int>* node2 = new Node<int>(4, node3, node4);
+    Node<int>* res = new Node<int>(2, node1, node2);
+    Tree<int> a(res);
+    if (a.contains(10)) {
+        cout << "True" << endl;
+    }
+    else cout << "False" << endl;
+}
+
+TEST(contain2, test) {
+    Tree<int> set;
+    Node<int>* node1 = new Node<int>(1);
+    Node<int>* node3 = new Node<int>(5);
+    Node<int>* node4 = new Node<int>(10);
+    Node<int>* node2 = new Node<int>(4, node3, node4);
+    Node<int>* res = new Node<int>(2, node1, node2);
+    Tree<int> a(res);
+    if (a.contains(-4)) {
+        cout << "True" << endl;
+    }
+    else cout << "False" << endl;
+}
